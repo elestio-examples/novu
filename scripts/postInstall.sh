ac3437015b4c46597b724803c6d16a611a4db5e1
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-18 11:32:58 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,16 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 30s;
+
+target=$(docker-compose port api 3000)
+
+
+curl http://${target}/v1/auth/register \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+  --data-raw '{"firstName":"admin","lastName":"admin","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'' \
+  --compressed
